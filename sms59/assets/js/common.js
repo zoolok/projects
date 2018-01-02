e814988867cd37660d7f92c752c9585618ea7f44
zoolok <mityasazonow@yandex.ru>
2018-01-02 18:05:48 +0300

    отдельный каталог под краны

--- sms59/assets/js/common.js
+++ sms59/assets/js/common.js
@@ -15,6 +15,21 @@ $('document').ready(function () {
         }
     });
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.arrow-up').fadeIn();
+        } else {
+            $('.arrow-up').fadeOut();
+        }
+    });
+
+    $('.arrow-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
     $('#letters').owlCarousel({
         items:1,
         nav:true,
