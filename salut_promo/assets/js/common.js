6b8d93b6eafead8f3a7d4c4df01df7a6b4f3d69f
zoolok <mityasazonow@yandex.ru>
2018-01-10 15:01:28 +0300

    Главная

--- salut_promo/assets/js/common.js
+++ salut_promo/assets/js/common.js
@@ -1,6 +1,38 @@
 $(document).ready(function () {
+
     $(window).scroll(function () {
             $('.shape').css('transform','translate('+ $(this).offset().top  +'%,' + $(this).scrollTop() /10 +'%)');
     });
 
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+    $('#letter').owlCarousel({
+        items:1,
+        nav:true,
+        navText:[],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+
+    });
 });
