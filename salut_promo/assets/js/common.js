e5cce1fa823ace8b3634ecf16512576ebe6d6e9f
zoolok <mityasazonow@yandex.ru>
2018-01-12 19:22:05 +0300

    Главная

--- salut_promo/assets/js/common.js
+++ salut_promo/assets/js/common.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-    $(window).scroll(function () {
+/*    $(window).scroll(function () {
             $('.shape').css('transform','translate('+ $(this).offset().top  +'%,' + $(this).scrollTop() /10 +'%)');
-    });
+    });*/
 
     $('.image-popup-no-margins').magnificPopup({
         type: 'image',
@@ -34,5 +34,45 @@ $(document).ready(function () {
             }
         }
 
+    });
+    $('.slider-adv').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-adv-nav'
+    });
+
+    $('.slider-adv-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-adv',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    var show = true;
+    var countbox = "#counts";
+    $(window).on("scroll load resize", function(){
+
+        if(!show) return false;
+
+        var w_top = $(window).scrollTop();
+        var e_top = $(countbox).offset().top;
+
+        var w_height = $(window).height();
+        var d_height = $(document).height();
+
+        var e_height = $(countbox).outerHeight();
+
+        if(w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+            $(".spincrement").spincrement({
+                thousandSeparator: "",
+                duration: 2500
+            });
+
+            show = false;
+        }
     });
 });
