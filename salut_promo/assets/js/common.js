676011f5a3374aa1ffe63c01023b3009967b71d3
zoolok <mityasazonow@yandex.ru>
2018-01-17 14:37:38 +0300

    Главная

--- salut_promo/assets/js/common.js
+++ salut_promo/assets/js/common.js
@@ -1,37 +1,67 @@
 $(document).ready(function () {
+    /* --------------------------------------------------------
+       MAGNIFIC POP-UP
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( ".image-popup-no-margins" ).length > 0 ) {
 
-    $('.image-popup-no-margins').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom',
-        image: {
-            verticalFit: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300
+            $('.image-popup-no-margins').magnificPopup({
+                type: 'image',
+                closeOnContentClick: true,
+                closeBtnInside: false,
+                fixedContentPos: true,
+                mainClass: 'mfp-no-margins mfp-with-zoom',
+                image: {
+                    verticalFit: true
+                },
+                zoom: {
+                    enabled: true,
+                    duration: 300
+                }
+            });
         }
     });
 
-    $('#letter').owlCarousel({
-        items:1,
-        nav:true,
-        navText:[],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
+    /* --------------------------------------------------------
+          FANCYBOX POP-UP
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( "[data-fancybox]" ).length > 0 ) {
+            $("[data-fancybox]").fancybox({
+                // Options will go here
+            });
         }
-
     });
+
+    /* --------------------------------------------------------
+         INDEX OWL CARUSEL
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( "#letter" ).length > 0 ) {
+
+            $('#letter').owlCarousel({
+                items:1,
+                nav:true,
+                navText:[],
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 1
+                    },
+                    1000: {
+                        items: 1
+                    }
+                }
+
+            });
+        }
+    });
+
 
     $('.other-slider').slick({
         infinite: true,
@@ -49,8 +79,8 @@ $(document).ready(function () {
         ]
     });
 
-$('.other-slider .slick-next').html('right');
-$('.other-slider .slick-prev').html('left');
+    $('.other-slider .slick-next').html('right');
+    $('.other-slider .slick-prev').html('left');
 
     $('.slider-adv').slick({
         slidesToShow: 1,
@@ -75,35 +105,74 @@ $('.other-slider .slick-prev').html('left');
         focusOnSelect: true
     });
 
-    var show = true;
-    var countbox = "#counts";
-    $(window).on("scroll load resize", function(){
+    /* --------------------------------------------------------
+                        MOBILE MENU
+----------------------------------------------------------- */
+    $('.m-menu').click(function () {
 
-        if(!show) return false;
+        $('.mobile-menu').animate({
+            left: '0'
+        }, 300);
 
-        var w_top = $(window).scrollTop();
-        var e_top = $(countbox).offset().top;
+        $('.mobile-menu').addClass('fixed');
+        $('.wrapper-content, footer').addClass('blured');
+    });
 
-        var w_height = $(window).height();
-        var d_height = $(document).height();
+    $('.mobile-menu span').click(function () {
+        $('.mobile-menu').animate({
+            left: "-999px"
+        }, 300);
 
-        var e_height = $(countbox).outerHeight();
+        $('.mobile-menu').removeClass('fixed');
+        $('.wrapper-content, footer').removeClass('blured');
+    });
 
-        if(w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
-            $(".spincrement").spincrement({
-                thousandSeparator: "",
-                duration: 2500
+    /* --------------------------------------------------------
+                        BLOG MIXITUP
+----------------------------------------------------------- */
+    $(function($) {
+        var mixer = mixitup('.blog-wrap', {
+            controls: {
+                toggleDefault: 'all',
+                toggleLogic: 'or'
+            }
+        });
+    });
+
+    /* --------------------------------------------------------
+                        INDEX COUNTS
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( "#counts" ).length > 0 ) {
+
+            var show = true;
+            var countbox = "#counts";
+
+            $(window).on("scroll load resize", function () {
+
+                if (!show) return false;
+
+                var w_top = $(window).scrollTop();
+                var e_top = $(countbox).offset().top;
+
+                var w_height = $(window).height();
+                var d_height = $(document).height();
+
+                var e_height = $(countbox).outerHeight();
+
+                if (w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+                    $(".spincrement").spincrement({
+                        thousandSeparator: "",
+                        duration: 2500
+                    });
+
+                    show = false;
+                }
             });
-
-            show = false;
         }
     });
-
-    var mixer = mixitup('.blog-wrap', {
-        controls: {
-            toggleDefault: 'all',
-            toggleLogic: 'or'
-        }
-    });
-
 });
+
+
+
