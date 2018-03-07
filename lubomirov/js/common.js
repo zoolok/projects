adab99d48f12b4556eb4630012a5f64b5d14b0c5
Sazonov <sazonov@iqgroup-nn.ru>
2018-03-07 15:59:55 +0300

    Главная

---
+++ lubomirov/js/common.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+
+    /* --------------------------------------------------------
+          FANCYBOX POP-UP
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( "[data-fancybox]" ).length > 0 ) {
+            $("[data-fancybox]").fancybox({
+                // Options will go here
+            });
+        }
+    });
+    /* --------------------------------------------------------
+         INDEX OWL CARUSEL
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $( ".main-slider" ).length > 0 ) {
+
+            $('.main-slider').owlCarousel({
+                items:1,
+                nav:true,
+                navText:["",""],
+                dots:true,
+                loop:false,
+                center:true,
+                dotsContainer: '.dots',
+                autoplay:true
+            });
+        }
+        $('.owl-dots').wrap("<div class='container'></div>");
+    });
+
+
+    /* --------------------------------------------------------
+                        MOBILE MENU
+----------------------------------------------------------- */
+
+    var menu = $('.mobile-menu');
+
+    $('.m-menu-button').click(function () {
+        menu.fadeToggle(300);
+        menu.addClass('fixed');
+    });
+    $('.m-close-menu').click(function () {
+        menu.fadeToggle(300);
+        menu.removeClass('fixed');
+    });
+});
