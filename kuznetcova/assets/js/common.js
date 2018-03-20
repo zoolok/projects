257d9efcb0d616c7733fe21db4a17a5cc52b21dc
Sazonov <sazonov@iqgroup-nn.ru>
2018-03-20 09:14:05 +0300

    Главная

---
+++ kuznetcova/assets/js/common.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    /* --------------------------------------------------------
+          FANCYBOX POP-UP
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $("[data-fancybox]").length > 0 ) {
+            $("[data-fancybox]").fancybox({
+                // Options will go here
+            });
+        }
+    });
+    /* --------------------------------------------------------
+         INDEX OWL CARUSEL
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ( $(".main-slider").length > 0 ) {
+
+            $('.main-slider').owlCarousel({
+                items:1,
+                nav:false,
+                dots:true,
+                loop:true,
+                center:true,
+                autoplay:true
+            });
+        }
+
+    });
+    /* --------------------------------------------------------
+                       PODDOMENY
+----------------------------------------------------------- */
+    $('#region').change(function (e) {
+        e.preventDefault();
+        var url = $('#region option:selected').val();
+        $(location).attr('href',url);
+    });
+    /* --------------------------------------------------------
+                        MOBILE MENU
+----------------------------------------------------------- */
+
+    var menu = $('.mob-menu');
+
+    $('.menu-button').click(function () {
+        menu.fadeToggle(300);
+        menu.addClass('fixed');
+    });
+    $('.m-close-menu').click(function () {
+        menu.fadeToggle(300);
+        menu.removeClass('fixed');
+    });
+
+
+});
