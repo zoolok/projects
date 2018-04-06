f017e7948905b4bcbe22f49f49a8f9b699dac146
Sazonov <sazonov@iqgroup-nn.ru>
2018-04-06 15:13:30 +0300

    Главная

--- _blank/mail.php
+++ _blank/mail.php
@@ -9,5 +9,10 @@ $email = trim($_POST["email"]);
 $comment = trim($_POST["comment"]);
 
 $pagetitle = "Заявка  с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nEmail: $email \nСообщение: $comment";
+$message =
+    "Имя: $name
+ \nТелефон: $phone
+  \nEmail: $email
+   \nСообщение: $comment";
+
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
