fffe6306039685d982c0f81885912eface892ce4
Sazonov <sazonov@iqgroup-nn.ru>
2018-04-28 15:35:41 +0300

    Главная

--- lovec_snov/assets/js/common.js
+++ lovec_snov/assets/js/common.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
         MASKED INPUT
 ----------------------------------------------------------- */
     $('#phone').mask("+7 (999) 999-99-99");
+    $("#datepicker").datepicker();
 
 });
 /* --------------------------------------------------------
@@ -32,4 +33,37 @@ var pos = nav.offset().top;
 $(window).scroll(function () {
     var fix = ($(this).scrollTop() >= pos) ? true : false;
     nav.toggleClass("fixed", fix);
+});
+/* --------------------------------------------------------
+       COUNT
+----------------------------------------------------------- */
+var curCount = parseInt($('#count').html());
+
+$('.ticket-count span:first-of-type').click(function () {
+    if ( curCount > 0){
+        $('#count').html(curCount --);
+    }else{
+        return;
+    }
+});
+
+$('.ticket-count span:last-of-type').click(function () {
+        $('#count').html(curCount ++);
+});
+/* --------------------------------------------------------
+      ANCOR
+----------------------------------------------------------- */
+$('a.ancor').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+    return false;
+});
+
+
+$("#datepicker").datepicker({
+    onSelect: function(dateText, inst) {
+        var arrayLimited = $(this).val().split("/", 3);
+        console.log(arrayLimited);
+        $("#datepicker").html('66654');
+    }
 });
