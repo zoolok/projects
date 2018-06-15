69adb9fb0097366c348a8e6358fbee6d9961a66b
Sazonov <sazonov@iqgroup-nn.ru>
2018-06-15 15:43:20 +0300

    88

--- autoligatrans/assets/js/common.js
+++ autoligatrans/assets/js/common.js
@@ -19,29 +19,16 @@ $(document).ready(function () {
         if ( $(".main-slider").length > 0 ) {
 
             $('.main-slider').owlCarousel({
-                items:1,
+                items:3,
                 nav:true,
                 navText:["",""],
-                dots:true,
+                dots:false,
                 loop:true,
                 center:true,
-                autoplay:true
+                autoplay:false
             });
         }
-        var foto = $(".slider-foto");
 
-        if ( foto.length > 0 ) {
-            foto.slick({
-                autoplay: true,
-                infinite: true
-            });
-        }
-        var vid = $(".slider-video");
-        if ( vid.length > 0 ) {
-            vid.slick({
-
-            });
-        }
     });
 
 
@@ -60,48 +47,6 @@ $(document).ready(function () {
         menu.removeClass('fixed');
     });
 
-    /* --------------------------------------------------------
-              SCROLL DOWN
------------------------------------------------------------ */
-    $('.down').click(function () {
-        var hs = $('.main').height();
-        /*console.log(hs);*/
-        $('html, body').stop().animate({
-            scrollTop: hs +'px'
-        },1000);
-    });
-    /* --------------------------------------------------------
-          FOTO VIDEO TABS
------------------------------------------------------------ */
-    $('.foto').click(function () {
-        $(this).addClass('active');
-        $('.slider-foto').addClass('visible');
-        $('.slider-foto').removeClass('hidden');
-        $('.video').removeClass('active');
-        $('.slider-video').removeClass('visible');
-        $('.slider-video').addClass('hidden');
-    });
-    $('.video').click(function () {
-        $(this).addClass('active');
-        $('.slider-video').addClass('visible');
-        $('.slider-video').removeClass('hidden');
-        $('.foto').removeClass('active');
-        $('.slider-foto').removeClass('visible');
-        $('.slider-foto').addClass('hidden');
-    });
-
-    /* --------------------------------------------------------
-          NEWS & BLOG MIXITUP
------------------------------------------------------------ */
-
-    if ($("#news-grid").length > 0) {
-        $("#news-grid").mixItUp();
-    }
-
-    $('.controls li').click(function () {
-        $(".controls li").removeClass("active");
-        $(this).addClass("active");
-    });
 
     /* --------------------------------------------------------
         MASKED INPUT
