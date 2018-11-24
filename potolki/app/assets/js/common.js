70bdcd097e4decce48702a1bf9ffc570f43975c8
zoolok <mityasazonow@yandex.ru>
2018-11-24 19:56:21 +0300

    Главная

---
+++ potolki/app/assets/js/common.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    /* --------------------------------------------------------
+       PRICE SLIDER
+----------------------------------------------------------- */
+    if ($("#price-slider").length >0){
+
+        $("#price-slider").slider({
+            value:25,
+            min: 0,
+            max: 50,
+            step: 1,
+            slide: function( event, ui ) {
+                $( "#amount" ).val(ui.value + ' кв.м' );
+                var arg =  parseInt(ui.value);
+                $( ".summa" ).html( number_format((arg*300),'','',' ') + ' - ' + number_format((arg*400),'','',' ')  + ' руб');
+            }
+        });
+
+        $('#amount').on("click",function () {
+            $(this).val("");
+            $("#price-slider").slider({value:0});
+        });
+
+        $('#amount').on("input",function () {
+            var arg =  $(this).val();
+            $("#price-slider").slider({value:arg});
+            $( ".summa" ).html( number_format((arg*300),'','',' ') + ' - ' + number_format((arg*400),'','',' ')  + ' руб');
+        });
+
+    }
+    /* --------------------------------------------------------
+    SLIDERS
+    ----------------------------------------------------------- */
+
+    $('#cascade-slider').cascadeSlider({
+        itemClass: 'cascade-slider_item',
+        arrowClass: 'cascade-slider_arrow'
+    });
+
+    /* --------------------------------------------------------
+PHONE MASK
+----------------------------------------------------------- */
+    $(function() {
+        "use strict";
+        if ($("#phone").length  || $("#phone-1").length ) {
+            $("#phone, #phone-1").mask("+7(999) 999-99-99");
+        }
+    })
+});
+
+/* --------------------------------------------------------
+NUMBER FORMAT
+----------------------------------------------------------- */
+number_format = function (number, decimals, dec_point, thousands_sep) {
+    number = number.toFixed(decimals);
+
+    var nstr = number.toString();
+    nstr += '';
+    x = nstr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? dec_point + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+
+    while (rgx.test(x1))
+        x1 = x1.replace(rgx, '$1' + thousands_sep + '$2');
+
+    return x1 + x2;
+};
+
---
+++ potolki/gulpfile.js
@@ -0,0 +1,98 @@
+var syntax        = 'css'; // Syntax: sass or css;
+
+var gulp       = require('gulp'), // Подключаем Gulp
+    browserSync  = require('browser-sync'), // Подключаем Browser Sync
+    concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+    cleancss      = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
+
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'app/assets/libs/jquery/dist/jquery.min.js', // jQuery
+        'app/assets/libs/cascade-slider/cascade-slider.js',// Glider Carousel
+        'app/assets/libs/jquery.maskedinput/dist/jquery.maskedinput.min.js', // Phone Mask
+        'app/assets/libs/jquery-ui-1.12.1/jquery-ui.min.js', // jQuery UI
+        'app/assets/libs/jquery-ui-1.12.1/jquery.ui.touch-punch.min.js' // jQuery UI Tuch
+    ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/assets/js')) // Выгружаем в папку app/js
+        .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+    return gulp.src([
+        'app/assets/css/main.css',
+        'app/assets/css/libs.css'
+    ])
+        .pipe(rename({ suffix: '.min', prefix : '' }))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.stream())
+});
+
+
+gulp.task('watch', ['styles', 'scripts', 'browser-sync'], function() {
+    gulp.watch('app/assets/'+syntax+'/**/*.'+syntax+'', ['styles']);
+    gulp.watch(['app/assets/libs/**/*.js', 'app/assets/js/**/*.js'], browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload)
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/assets/images/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/assets/images')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['clean', 'img', 'scripts'], function() {
+
+    var buildCss = gulp.src([ // Переносим стили в продакшен
+        'app/assets/css/main.min.css',
+        'app/assets/css/libs.min.css'
+    ])
+        .pipe(gulp.dest('dist/assets/css'))
+
+    var buildFonts = gulp.src('app/assets/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('dist/assets/fonts'))
+
+    var buildJs = gulp.src('app/assets/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('dist/assets/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+        .pipe(gulp.dest('dist'));
+
+
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+});
+
+gulp.task('default', ['watch']);
